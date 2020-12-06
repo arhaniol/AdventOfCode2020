187f3a3de8881ac39e1bf0dadcc788af0d2d28b9
arhaniol <arhaniol@op.pl>
2020-12-06 23:37:38 +0100

    Improved day1 and day2. Solved day3. day4 in progress.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,15 +2,35 @@ package org.example;
 
 import org.example.day1.ReportRepair;
 import org.example.day2.PasswordPhilosophy;
+import org.example.day3.TobogganTrajectory;
+import org.example.day4.PassportProcessing;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-//        ReportRepair reportRepair=new ReportRepair();
-//        System.out.println(reportRepair.findMultiplicationOfSum());
+        System.out.println("Day 1:");
+        ReportRepair reportRepair = new ReportRepair();
+        reportRepair.findMultiplicationOfSum();
+        System.out.println("Part1: " + reportRepair.getPart1());
+        System.out.println("Part2: " + reportRepair.getPart2());
 
+        System.out.println("Day 2:");
         PasswordPhilosophy passwordPhilosophy = new PasswordPhilosophy();
-        System.out.println(passwordPhilosophy.countValidPassword());
+        passwordPhilosophy.countValidPassword();
+
+        System.out.println("* Part1: " + passwordPhilosophy.getPart1());
+        System.out.println("* Part2: " + passwordPhilosophy.getPart2());
+
+        System.out.println("Day 3:");
+        TobogganTrajectory tobogganTrajectory = new TobogganTrajectory("input.txt");
+
+        System.out.println("* Part1: " + tobogganTrajectory.getPart1());
+        System.out.println("* Part2: " + tobogganTrajectory.getPart2());
+
+        System.out.println("Day 4:");
+        PassportProcessing passportProcessing = new PassportProcessing("input.txt");
+
+        System.out.println("* Part1: " + passportProcessing.getPart1());
+//        System.out.println("* Part2: " + passportProcessing.getPart2());
     }
 
 }
