005e8a5192038d94d1c03989765b4ee8c8b77706
arhaniol <arhaniol@op.pl>
2020-12-14 00:19:51 +0100

    day7 solution

--- src/main/java/org/example/day7/Bag.java
+++ src/main/java/org/example/day7/Bag.java
@@ -1,56 +1,59 @@
 package org.example.day7;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Bag {
-    private String color;
-    private List<Bag> content;
+    private String name;
+    private List<Bag> parents;
+    private Map<String,Integer> children;
+
+    public boolean addParent(Bag parent){
+        if(!parents.contains(parent)){
+            parents.add(parent);
+            return true;
+        }
+        return false;
+    }
 
     public Bag() {
-        content = new ArrayList<>();
+        parents=new ArrayList<>();
+        children =new HashMap<>();
     }
 
-    public Bag(String color, List<Bag> content) {
-        this.color = color;
-        if (content == null) {
-            this.content = null;
-        } else {
-            this.content = new ArrayList<>(content);
-        }
+    public String getName() {
+        return name;
     }
 
-    public String getColor() {
-        return color;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public List<Bag> getParents() {
+        return parents;
     }
 
-    public void setContent(List<Bag> content) {
-        this.content = content;
+    public void setParents(List<Bag> parents) {
+        this.parents = parents;
     }
 
-    public void addToContent(Bag bag) {
-        if (bag != null && content != null) {
-            content.add(bag);
-        } else {
-            content = null;
-        }
+    public Map<String, Integer> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Map<String, Integer> children) {
+        this.children = children;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Bag)) return false;
         Bag bag = (Bag) o;
-        return Objects.equals(color, bag.color);
+        return name.equals(bag.name) && Objects.equals(children, bag.children);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(color);
+        return Objects.hash(name, children);
     }
 }
