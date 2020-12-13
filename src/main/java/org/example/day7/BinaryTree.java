2c85772315eb5cfc438b1fe946c503b65b6e2be6
arhaniol <arhaniol@op.pl>
2020-12-14 00:21:22 +0100

    day7 solution

--- src/main/java/org/example/day7/BinaryTree.java
+++ src/main/java/org/example/day7/BinaryTree.java
@@ -1,26 +1,57 @@
 package org.example.day7;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree {
-    private Node root;
-    private List<Bag> bagList;
+    List<Bag> nodes;
 
-    public BinaryTree(){
-        root=new Node();
-        bagList=new LinkedList<>();
+    public BinaryTree() {
+        nodes = new ArrayList<>();
     }
-    public void add(Bag bag) {
-        if (bag != null) {
-            if(bagList.size()==0) {
-                bagList.add(bag);
-            }else{
 
+    public void addNode(Bag bag) {
+        if (nodes.contains(bag)) {
+            throw new IllegalArgumentException("Cos jest nie tak: " + bag.getName() + " już istnieje!");
+        }
+        for (Bag node : nodes) {
+            if (node.getChildren().containsKey(bag.getName())) {
+                bag.addParent(node);
+            }
+            if (bag.getChildren().containsKey(node.getName())) {
+                node.addParent(bag);
             }
         }
+        nodes.add(bag);
     }
 
+    public Set<Bag> getAllParents(String nodeName) {
+        Set<Bag> bags = new HashSet<>();
+        if (nodeName == null || nodeName.length() == 0) {
+            return bags;
+        }
+        for (Bag node : nodes) {
+            if (node.getName().equals(nodeName)) {
+                bags.addAll(node.getParents());
+                for (Bag bag : node.getParents()) {
+                    bags.addAll(getAllParents(bag.getName()));
+                }
+            }
+        }
+        return bags;
+    }
 
-//    public void add()
+    public int getAllChildren(String rootName) {
+        if (rootName == null || rootName.length() == 0) {
+            return 0;
+        }
+        int counter = 1;
+        for (Bag node : nodes) {
+            if (node.getName().equals(rootName)) {
+                for (String name : node.getChildren().keySet()) {
+                    counter += node.getChildren().get(name) * getAllChildren(name);
+                }
+            }
+        }
+        return counter;
+    }
 }
